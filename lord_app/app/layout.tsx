186a19226c6f149ea862f19f5989ffcd930a482d
bharatlal <bharatlal.kumar@technians.com>
2025-12-16 16:19:23 +0530

    add navbar and swiper with logo

--- lord_app/app/layout.tsx
+++ lord_app/app/layout.tsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import React from "react";
->>>>>>> 13a2c7f082d70f0913d617f58258340e650489cc
 import "./globals.css";
 
 export default function RootLayout({
