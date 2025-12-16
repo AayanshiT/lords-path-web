7bff56a23469a5d33210442790f1f34001f5195c
technians-git <mritunjay.paswan@technians.com>
2025-12-16 18:47:37 +0530

    service card

--- lord_app/app/layout.tsx
+++ lord_app/app/layout.tsx
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-=======
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import React from "react";
->>>>>>> 13a2c7f082d70f0913d617f58258340e650489cc
-import "./globals.css";
+import "@/app/globals.css";
 
 export default function RootLayout({
   children,
