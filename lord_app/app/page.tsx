f2eff13737041e02ba1c8892ef50c5f375d12dfd
AayanshiT <aayanshi.sharma@technians.com>
2025-12-26 18:35:18 +0530

    changes

--- lord_app/app/page.tsx
+++ lord_app/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
       <ImageSection />
       <MobileSection />
       <MidSection />
-      <LabTests />
+      {/* <LabTests /> */}
       <HealthiansReviews />
       <AwardCards />
       <HealthBlogs />
