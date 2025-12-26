aec7c47e5bef18d8cf2a80773863958ed516b14f
AayanshiT <aayanshi.sharma@technians.com>
2025-12-26 13:33:06 +0530

    resolve error

--- lord_app/app/login/otp/page.tsx
+++ lord_app/app/login/otp/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import HealthiansOTP from '@/components/Login/otp';
 import TopBar from '@/components/TopBar';
 import Footer from '@/components/Footer';
@@ -5,14 +8,21 @@ import FooterSection from '@/components/FooterSection';
 import Bottom from '@/components/bottom';
 
 export default function LoginPage() {
+  const [showOTP, setShowOTP] = useState(false);
+  const [mobileNumber, setMobileNumber] = useState('+91 9116901768');
+
   return (
     <>
       <TopBar />
-      <HealthiansOTP />
+
+        <HealthiansOTP
+          phone={mobileNumber}
+          onBack={() => setShowOTP(false)} // Go back to number input
+        />
+
       <FooterSection />
       <Footer />
       <Bottom />
     </>
-
   );
 }
