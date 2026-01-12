fdc58b3896db49eae80a66e9b753571d6c077575
technians-git <mritunjay.paswan@technians.com>
2026-01-12 11:12:32 +0530

    checkout changes

--- lord_app/app/checkout/page.tsx
+++ lord_app/app/checkout/page.tsx
@@ -1,14 +1,18 @@
+import { Suspense } from "react";
 import Checkout from '@/components/CheckoutPage/checkout';
 import TopBar from '@/components/TopBar';
 import Footer from '@/components/Footer';
 import FooterSection from '@/components/FooterSection';
 import Bottom from '@/components/bottom';
 
+
 export default function LoginPage() {
   return (
     <>
       <TopBar />
-      <Checkout />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Checkout />
+      </Suspense>
       <FooterSection />
       <Footer />
       <Bottom />
